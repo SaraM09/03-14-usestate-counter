4fe2e643803768768855b942a957707c6ce3931a
SaraM09 <sayeramusfika@pursuit.org>
2024-01-06 13:47:15 -0500

    Sara's 03-14-usestate-counter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import StatefulCounter from './StatefulCounter';
+function App() {
+  return (
+    <div className="App">
+    <StatefulCounter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StatefulCounter.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function StatefulCounter() {
+    const [count, setCount] = useState(0);
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </>
+    );
+}
+
+export default StatefulCounter;
